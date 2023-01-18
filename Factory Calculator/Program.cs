a054eb8cc1b249edfc8df7cad1943f8585a8b768
BuildTools <unconfigured@null.spigotmc.org>
2023-01-18 03:32:01 -0600

    init

--- Factory Calculator/Program.cs
+++ Factory Calculator/Program.cs
@@ -1,33 +1,87 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace Factory_Calculator
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            for(int i = 0; i<args.Length; i ++) 
-            {
-                switch (args[i])
-                {
-                    case "/f":
-                        if (args.Length > i + 1)
-                        {
-                            Console.WriteLine($"Reading file: {args[i + 1]}");
-                            i++;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Error: /f Flag used but no file name given!");
-                            Environment.Exit(1);
-                        }
-                        break;
-                    default:
-                        break;
-                }
-            }
+	public class Item
+	{
+		public string ItemName { get; set; }
+		public Dictionary<String, int> Ingredients { get; set; }
+		public float? Craft_Time { get; set; }
+		public int? Amount { get; set; }
+	}
+
+	class Program
+	{
+		public static Dictionary<string, Item> Items { get; set; }
+		public static Dictionary<string, float> ItemFactoryTotals { get; set; }
+
+		static void Main(string[] args)
+		{
+			ItemFactoryTotals = new Dictionary<string, float>();
+
+			string filename = args.Length == 0 ? "settings.json" : args[0];
+
+			if (File.Exists(filename))
+			{
+				string jsonText = File.ReadAllText(filename);
+				try
+				{
+					Items = JsonSerializer.Deserialize<Dictionary<string, Item>>(jsonText)!;
+
+					Console.WriteLine("What do you want to craft?");
+					string itemToCraft = Console.ReadLine().Trim().ToLower();
+
+					Calculate(Items[itemToCraft]);
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine(e);
+					Environment.Exit(1);
+				}
+			}
+			else
+			{
+				Console.WriteLine("File does not exist! (default file name: settings.json)");
+				Environment.Exit(1);
+			}
         }
+
+		static void Calculate(Item itemToCraft, bool init = true)
+        {
+			foreach(KeyValuePair<string, int> kvp in itemToCraft.Ingredients)
+            {
+				if (Items[kvp.Key].Ingredients != null)
+				{
+					Item crafting = Items[kvp.Key];
+					float ratio = (float)((kvp.Value * crafting.Craft_Time)/ crafting.Amount / itemToCraft.Craft_Time);
+
+                    if (ItemFactoryTotals.ContainsKey(kvp.Key))
+                    {
+						ItemFactoryTotals[kvp.Key] += ratio;
+                    }
+                    else
+                    {
+						ItemFactoryTotals.Add(kvp.Key, ratio);
+                    }
+
+					Calculate(crafting, false);
+				}
+            }
+
+            if (init)
+            {
+				foreach (KeyValuePair<string, float> kvp in ItemFactoryTotals)
+				{
+					Console.WriteLine($"{kvp.Key} machines needed: {MathF.Ceiling(kvp.Value)}");
+				}
+				Console.WriteLine("\nPress Any Key To Continue...");
+				Console.ReadKey();
+			}
+        }
+
     }
 }
